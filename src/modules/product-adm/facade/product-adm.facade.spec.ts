830284a675475fb7e7539cd7957976f191ebbe19
Raphael Berger <raphaelmb@gmail.com>
2022-08-19 11:12:59 -0300

    Add product-adm facade

---
+++ src/modules/product-adm/facade/product-adm.facade.spec.ts
@@ -0,0 +1,49 @@
+import { Sequelize } from "sequelize-typescript";
+import ProductModel from "../repository/product.model";
+import ProductRepository from "../repository/product.repository";
+import AddProductUseCase from "../usecase/add-product/add-product.usecase";
+import ProductAdmFacade from "./product-adm.facade";
+
+describe("ProductAdmFacade test", () => {
+  let sequelize: Sequelize;
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+    sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("should create a product", async () => {
+    const productRepository = new ProductRepository();
+    const addProductUseCase = new AddProductUseCase(productRepository);
+    const productFacade = new ProductAdmFacade({
+      addUseCase: addProductUseCase,
+      stockUseCase: undefined,
+    });
+
+    const input = {
+      id: "1",
+      name: "Product 1",
+      description: "Product 1 description",
+      purchasePrice: 100,
+      stock: 10,
+    };
+
+    await productFacade.addProduct(input);
+    const product = await ProductModel.findOne({ where: { id: "1" } });
+    expect(product).toBeDefined();
+    expect(product.id).toEqual(input.id);
+    expect(product.name).toEqual(input.name);
+    expect(product.description).toEqual(input.description);
+    expect(product.purchasePrice).toEqual(input.purchasePrice);
+    expect(product.stock).toEqual(input.stock);
+  });
+});
