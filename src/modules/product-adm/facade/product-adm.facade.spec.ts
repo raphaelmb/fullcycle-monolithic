2e5203f7ec5bf19265a2b32ec5c254457757fb8d
Raphael Berger <raphaelmb@gmail.com>
2022-08-22 15:49:32 -0300

    Add checkstock usecase

--- src/modules/product-adm/facade/product-adm.facade.spec.ts
+++ src/modules/product-adm/facade/product-adm.facade.spec.ts
@@ -1,9 +1,6 @@
 import { Sequelize } from "sequelize-typescript";
 import ProductAdmFacadeFactory from "../factory/facade.factory";
 import ProductModel from "../repository/product.model";
-import ProductRepository from "../repository/product.repository";
-import AddProductUseCase from "../usecase/add-product/add-product.usecase";
-import ProductAdmFacade from "./product-adm.facade";
 
 describe("ProductAdmFacade test", () => {
   let sequelize: Sequelize;
@@ -41,5 +38,22 @@ describe("ProductAdmFacade test", () => {
     expect(product.description).toEqual(input.description);
     expect(product.purchasePrice).toEqual(input.purchasePrice);
     expect(product.stock).toEqual(input.stock);
+  });
+
+  it("should check a product stock", async () => {
+    const productFacade = ProductAdmFacadeFactory.create();
+
+    const input = {
+      id: "1",
+      name: "Product 1",
+      description: "Product 1 description",
+      purchasePrice: 100,
+      stock: 10,
+    };
+
+    await productFacade.addProduct(input);
+    const result = await productFacade.checkStock({ productId: "1" });
+    expect(result.productId).toBe(input.id);
+    expect(result.stock).toBe(input.stock);
   });
 });
