d2da150ecdacfeb3bc602a877e1f1c9fe9f2dd9a
Raphael Berger <raphaelmb@gmail.com>
2022-08-23 14:09:34 -0300

    Add store catalog entity + findall usecase

---
+++ src/modules/store-catalog/usecase/find-all-products/find-all-products.usecase.ts
@@ -0,0 +1,19 @@
+import UseCaseInterface from "../../../@shared/usecase/use-case.interface";
+import Product from "../../domain/product.entity";
+import ProductGateway from "../../gateway/product.gateway";
+import { FindAllProductsDto } from "./find-all-products.dto";
+
+export default class FindAllProductsUseCase implements UseCaseInterface {
+  constructor(private productRepository: ProductGateway) {}
+  async execute(): Promise<FindAllProductsDto> {
+    const products = await this.productRepository.findAll();
+    return {
+      products: products.map((product) => ({
+        id: product.id.id,
+        name: product.name,
+        description: product.description,
+        salesPrice: product.salesPrice,
+      })),
+    };
+  }
+}
